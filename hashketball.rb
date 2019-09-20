c81d0506a0dc82a9f1c0c8a6fd240bbe973cf4e5
Gina Graci <ginagraci92@gmail.com>
2019-09-20 21:12:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,6 +4,36 @@ def hashketball = {
     :team_name => "Brooklyn Nets"
     :colors => ["Black", "White"]
     :players => {
+      {
+      
+      "Alan Anderson" => {
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+        }
+      },
+
+      "Reggie Evans" => {
+        
+        }
+      },
+
+      "Bang" => {
+        :number => 93,
+        :shoe_size => 12,
+        :stats => {
+          :points => 24,
+          :rebounds => 9,
+          :assists => 13,
+          :steals => 10,
+          :blocks => 6,
+          :slam_dunks => 3
+        }
       
     }
    }
