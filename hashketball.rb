599ace7d552fb889ee267e32a563cf8c50d55384
Gina Graci <ginagraci92@gmail.com>
2019-09-20 21:06:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,9 @@
 # Write your code here!
 def hashketball = {
   :home => {
-    :team_name =>
-    :colors =>
-    :players =>
+    :team_name => "Brooklyn Nets"
+    :colors => ["Black", "White"]
+    :players => 
    }
   }
   
