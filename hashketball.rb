100fb76e4087d973d8f0758fe565d3067e8dfe76
Gina Graci <ginagraci92@gmail.com>
2019-09-20 21:23:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -49,7 +49,40 @@ def hashketball = {
       :team_name => "Charlotte Hornets"
     :colors => ["Turquoise", "Purple"]
     :players => {
-  
+      
+  "Jeff Adrien" => {
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2 
+        }
+      },
+
+      "Bismack Biyombo" => {
+        :number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10 
+        }
+      },
+
+      "DeSagna Diop" => {
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 1,
+        :slam_dunks => 15
 }
 
 
